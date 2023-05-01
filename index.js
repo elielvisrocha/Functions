44fffba6c1e04c786d8a39432a081087f1243248
elielvisrocha <rochaelielvis@gmail.com>
2023-05-01 19:16:14 -0300

    default parameters

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 'use strict';
 
 
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 500, price = 10 * numPassengers) {
+
+    // numPassengers = numPassengers || 1;
+    // price = price || 500;
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    };
+
+    console.log(booking);
+    bookings.push(booking);
+
+}
+
+
+createBooking('LH123', 23);
+createBooking('KL321', 78);
